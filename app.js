072c81dd788e52723a3a021035e973d2b79f3314
Roshan Yadav <62638119+roshanyadav1793@users.noreply.github.com>
2020-12-09 19:42:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const https=require('https');
 const ejs=require('ejs');
+const mongoose=require('mongoose');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocalMongoose =require('passport-local-mongoose');
+const LocalStrategy = require('passport-local');
 
 const app=express();
+
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine','ejs');
+
+app.use(session({
+    secret:"our little secret",
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/weatherDB",{ useNewUrlParser: true ,useUnifiedTopology: true });
+mongoose.set("useCreateIndex",true);
 
 app.get("/",(req,res)=>{
     res.render("index",{cssa:'index'});
@@ -20,9 +38,7 @@ app.get("/SignUp", function(req,res){
 app.get("/Login", function(req,res){
     res.render("login",{cssa:'login'});
 });
-app.get("/input", function(req,res){
-    res.render("input",{cssa:'index'});
-});
+
 
 app.post("/input", function(req,res){
     const query=req.body.cityName;
@@ -41,15 +57,61 @@ app.post("/input", function(req,res){
             const weatherDescription=weatherData.weather[0].description;
             const icon=weatherData.weather[0].icon;
             const imageURL="http://openweathermap.org/img/wn/"+ icon+"@2x.png";
+            res.setHeader('Content-Type', 'text/html');
             res.write("<p>The weather is currently " + weatherDescription+"</p>");
             res.write("<h1>The temperature in "+ query + " is "+ temp+" degrees celcius.</h1>");
             res.write("<img src="+ imageURL+">");
-            res.send();
-            res.set('Content-Type', 'text/html');
-        });
+            //res.write('<div class="card" style="width: 18rem;"><img class="card-img-top" src="..." alt="Card image cap"><div class="card-body"><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card content.</p></div></div>');
+            //  res.render("temp",{query: query, weatherDescription: weatherDescription,temperature: temp, imageURL: imageURL});            
+        }); 
     
     });
 
+});
+
+const userSchema=new mongoose.Schema({
+    email: {
+        type:String,
+        required: [true, "Please check your data entry, email not specified."]
+    },
+    password:{
+        type:String
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User= mongoose.model("User",userSchema);
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/input", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("input",{cssa:'input'});
+    }else{
+        res.redirect("/input");
+    }
+});
+
+
+app.post("/signup",function(req,res){   
+    User.register({email:req.body.username},req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            console.log(req.body.username);
+            res.redirect("/signup");
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/input");
+            });
+        }
+    });
+});
+
+app.post("/login",(req,res)=>{
+    
 });
  
 
